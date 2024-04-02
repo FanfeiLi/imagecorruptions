0f694c8b150757833012f910c99d50603da16baa
Fanfei Li <fanfei>
2024-04-02 11:50:09 +0200

    modified typo

--- imagecorruptions_new/__init__.py
+++ imagecorruptions_new/__init__.py
@@ -42,7 +42,7 @@ def corrupt_new(image, severity=1, corruption_name=None, corruption_number=-1,mi
         raise AttributeError('Expecting image.shape to be either (height x width) or (height x width x channels)')
     if image.ndim == 2:
         image = np.stack((image,)*3, axis=-1)
-        height, width, channels = image.shape
+    height, width, channels = image.shape
     if min_size_check:
         
         if (height < 32 or width < 32):
